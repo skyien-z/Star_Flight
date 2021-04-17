9721404b1b5d7f9aa822abfe792b3d7350635a50
skyien-z <kyliezh72@gmail.com>
2021-04-17 18:20:58 -0500

    Declare edge helper functions in StarInitializer

--- Star.cpp
+++ Star.cpp
@@ -1,3 +1,4 @@
 #include "Star.h"
 
-Star::Star(int id, std::string name, double x, double y, double z) : id(id), name(name), x(x), y(y), z(z){ }
+Star::Star(int id, std::string name, double x, double y, double z) 
+: id(id), name(name), x(x), y(y), z(z) { }
