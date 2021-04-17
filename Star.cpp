926f3eb5a3d8200a2a6061cc65e3c5f4430b0ebf
skyien-z <kyliezh72@gmail.com>
2021-04-17 18:25:19 -0500

    Implement star getter methods

--- Star.cpp
+++ Star.cpp
@@ -1,4 +1,24 @@
 #include "Star.h"
 
 Star::Star(int id, std::string name, double x, double y, double z) 
-: id(id), name(name), x(x), y(y), z(z) { }
+: id_(id), name_(name), x_(x), y_(y), z_(z) {}
+
+double Star::GetStarX() const {
+    return x_;
+}
+
+double Star::GetStarY() const {
+    return y_;
+}
+
+double Star::GetStarZ() const {
+    return z_;
+}
+
+int Star::GetStarId() const {
+    return id_;
+}
+
+const std::string& Star::GetStarName() const {
+    return name_;
+}
--- Star.h
+++ Star.h
@@ -3,11 +3,20 @@
 #include <string>
 
 class Star {
-    public:
-        Star(int id, std::string name, double x, double y, double z);
-        int id;
-        std::string name;
-        double x;
-        double y;
-        double z;
+  public:
+    Star(int id, std::string name, double x, double y, double z);
+
+    double GetStarX() const;
+    double GetStarY() const;
+    double GetStarZ() const;
+
+    int GetStarId() const;
+    const std::string& GetStarName() const;
+
+  private:
+    int id_;
+    std::string name_;
+    double x_;
+    double y_;
+    double z_;
 };
