e18a32c6ea9e0c1d55db1bfbc70f112bdf113636
keesanpatel <kpate360@illinois.edu>
2021-04-20 20:20:33 -0500

    started bfs

---
+++ bfs.cpp
@@ -0,0 +1,25 @@
+#include "bfs.h"
+#include <queue>
+
+BFS::BFSsmall(Star* star) {
+    std::queue<Star*> queue;
+    queue.push(star);
+
+    
+
+
+
+
+}
+
+
+BFS::BFStraversal(const std::vector<Star>& star_list_) {// call bfs small on every vertex
+
+}
+
+
+BFS::canTravel(Star* start, Star* end) {
+
+
+
+}
---
+++ bfs.h
@@ -0,0 +1,20 @@
+#include "Star.h"
+#include <vector>
+
+
+class BFS {
+    public:
+    BFSsmall(Star* star); // traversal from one start point
+
+    BFStraversal(const std::vector<Star>& star_list_); // call bfs small on every vertex
+
+
+
+    canTravel(Star* start, Star* end);
+
+
+
+
+
+
+};
