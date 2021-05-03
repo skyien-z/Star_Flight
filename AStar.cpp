18692e2ddb53985d092613c0514b24d2d3de5d8f
Charles Shi <charlesshi@Charless-MacBook-Air.local>
2021-05-03 17:21:41 -0500

    Add comments for the A* class

--- AStar.cpp
+++ AStar.cpp
@@ -18,11 +18,16 @@ double Distance(Star *current, Star *end)
                 (current->GetZ() - end->GetZ()) * (current->GetX() - end->GetZ()));
 }
 
+
 vector<string> AStar::findShortestPathAStar(Star *begin, Star *end)
 {
+    // keep track of the parent stars
     unordered_map<string, string> parent;
-    
+
+    // keep track of the final path
     vector<string> finalPath;
+
+    // create min-heap for the next nodes to visit based on the smallest costs
     priority_queue<pair<double, Star *>, vector<pair<double, Star *>>, std::greater<pair<double, Star *>>> nextNodes;
 
     nextNodes.emplace(0.0, begin);
@@ -31,15 +36,24 @@ vector<string> AStar::findShortestPathAStar(Star *begin, Star *end)
     {
         Star *currentStar = nextNodes.top().second;
         nextNodes.pop();
+
+        // end search if we've hit the destination
         if (currentStar == end)
         {
             break;
         }
+
+        // loop through the neighboring stars
         for (auto neighbor : currentStar->GetNeighboringStarsList())
         {
+
+            // make sure to not revisit stars
             if (!parent.count(neighbor.first->GetStarName()))
             {
+
                 parent[neighbor.first->GetStarName()] = currentStar->GetStarName();
+
+                // compute heuristic and edge weight
                 double neighborCost = neighbor.second + Distance(neighbor.first, end);
                 nextNodes.emplace(neighborCost, neighbor.first);
             }
@@ -47,11 +61,14 @@ vector<string> AStar::findShortestPathAStar(Star *begin, Star *end)
     }
 
     string temp = end->GetStarName();
+
+    // backtrack our steps from the end to the beginning to obtain the final path
     while (temp != begin->GetStarName())
     {
         finalPath.push_back(temp);
         temp = parent[temp];
     }
+    
     finalPath.push_back(temp);
     std::reverse(finalPath.begin(), finalPath.end());
 
--- AStar.h
+++ AStar.h
@@ -5,5 +5,12 @@
 
 class AStar {
     public:
+        /**
+         * @brief Executes the A* pathfinding algorithm, generating the shortest path from the begin star to the end star
+         * 
+         * @param begin the starting destination of the A* algorithm
+         * @param end the ending destination of the A* algorithm
+         * @return a vector that contains the shortest path of stars from the start to end destination
+         */
         std::vector<std::string> findShortestPathAStar(Star* begin, Star* end);
 };
