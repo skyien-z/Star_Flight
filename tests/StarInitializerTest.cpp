5acdb148a81a1dd241f2b86477816126c58fdd1b
skyien-z <kyliezh72@gmail.com>
2021-05-06 16:44:55 -0500

    Test StarInitializer Rule of 3

--- tests/StarInitializerTest.cpp
+++ tests/StarInitializerTest.cpp
@@ -33,6 +33,56 @@ TEST_CASE("Test Loading in Correct Data", "[weight=1]") {
   }
 }
 
+TEST_CASE("Test Copy Constructor", "[weight=1]") {
+  StarInitializer test_initializer("test_data/test_data.csv", 200);
+
+  StarInitializer new_intializer(test_initializer);
+  
+  // loaded_stars are the stars of the StarInitializer created by the copy constructor
+  std::vector<Star*> loaded_stars = new_intializer.GetStarList();
+
+  REQUIRE(loaded_stars.size() == 5);
+
+  std::vector<int> star_ids{0, 676, 744, 1065, 2076};
+  std::vector<std::string> star_names{"Sol", "Alpheratz", "Caph", "Algenib", "Ankaa"};
+  std::vector<double> x_coordinates{0, 25.974572, 8.600014, 115.664183, 19.083654};
+  std::vector<double> y_coordinates{0, 0.951042, 0.344589, 6.687324, 2.198282};
+  std::vector<double> z_coordinates{0, 14.461264, 14.409503, 31.442127, -17.483284};
+
+  for (int i = 0; i < 5; i++) {
+      REQUIRE(loaded_stars[i] -> GetStarName() == star_names[i]);
+      REQUIRE(loaded_stars[i] -> GetStarId() == star_ids[i]);
+      REQUIRE(loaded_stars[i] -> GetX() == x_coordinates[i]);
+      REQUIRE(loaded_stars[i] -> GetY() == y_coordinates[i]);
+      REQUIRE(loaded_stars[i] -> GetZ() == z_coordinates[i]);
+  }
+}
+
+TEST_CASE("Test Copy Assignment Operator", "[weight=1]") {
+  StarInitializer test_initializer("test_data/test_data.csv", 200);
+
+  StarInitializer new_intializer = test_initializer;
+
+  // loaded_stars are the stars of the StarInitializer created by the copy constructor
+  std::vector<Star*> loaded_stars = new_intializer.GetStarList();
+
+  REQUIRE(loaded_stars.size() == 5);
+
+  std::vector<int> star_ids{0, 676, 744, 1065, 2076};
+  std::vector<std::string> star_names{"Sol", "Alpheratz", "Caph", "Algenib", "Ankaa"};
+  std::vector<double> x_coordinates{0, 25.974572, 8.600014, 115.664183, 19.083654};
+  std::vector<double> y_coordinates{0, 0.951042, 0.344589, 6.687324, 2.198282};
+  std::vector<double> z_coordinates{0, 14.461264, 14.409503, 31.442127, -17.483284};
+
+  for (int i = 0; i < 5; i++) {
+      REQUIRE(loaded_stars[i] -> GetStarName() == star_names[i]);
+      REQUIRE(loaded_stars[i] -> GetStarId() == star_ids[i]);
+      REQUIRE(loaded_stars[i] -> GetX() == x_coordinates[i]);
+      REQUIRE(loaded_stars[i] -> GetY() == y_coordinates[i]);
+      REQUIRE(loaded_stars[i] -> GetZ() == z_coordinates[i]);
+  }
+}
+
 TEST_CASE("Test Computing Distance Between Stars", "[weight=1]") {
   StarInitializer test_initializer("test_data/test_data.csv", 200);
   std::vector<Star*> loaded_stars = test_initializer.GetStarList();
