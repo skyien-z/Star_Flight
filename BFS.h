90584cae02f5297d4d6181f33c2171641de5946b
Sriram Sridhar <srirams5@siebl-0224-16.ews.illinois.edu>
2021-05-06 18:30:57 -0500

    commented BFS header file

--- BFS.h
+++ BFS.h
@@ -5,9 +5,28 @@
 
 class BFS {
     public:
-        std::vector<Star*> StarTraversal(Star* star); // traversal from one start point
+        /**
+        * @brief Performs a BFS traversal starting from one star over all the stars it is connected to.
+        *
+        * @param star Pointer to the star that will be the start of the traversal
+        * @return Vector of star pointers in the traversal.
+        */
+        std::vector<Star*> StarTraversal(Star* star);
 
-        std::vector<Star*> GraphTraversal(const std::vector<Star*> nodeList); // call bfs small on every vertex
+        /**
+        * @brief Performs a BFS traversal over the whole graph by calling StarTraversal on every star that has not been visited.
+        *
+        * @param nodeList Vector of star pointers that will be travered via BFS (starting from the first star in the vector).
+        * @return Vector of star pointers in the full traversal.
+        */
+        std::vector<Star*> GraphTraversal(const std::vector<Star*> nodeList);
 
+        /**
+        * @brief Checks if a star has a path to another star.
+        *
+        * @param start Pointer to the first star.
+        * @param end Pointer to the destination star.
+        * @return Whether or not a path from the first star to the destination star exists.
+        */
         bool canTravel(Star* start, Star* end); //check if there is a path between two stars
 };
